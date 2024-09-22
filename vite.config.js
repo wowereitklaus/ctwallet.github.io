20d93dd55cd435789ed444875cb497d35f41a03b
wowereitklaus <cody@acom-networks.com>
2024-09-22 14:38:13 +0800

    Updated: "cname"

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
@@ -14,5 +13,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    target: 'es2015', // This ensures compatibility with older browsers
   },
 })
